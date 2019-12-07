0788a4d39a693891f116eefd12eaad616f2e1249
So Koide <sokoide@gmail.com>
2019-12-07 17:32:08 +0900

    Changed to use alpine image

--- nginx-spnego/data/nginx/nginx-start.sh
+++ nginx-spnego/data/nginx/nginx-start.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 cp /data/nginx/conf/nginx.conf /usr/local/nginx/conf
 cp /data/etc/krb5.conf /etc/krb5.conf
 cp /data/etc/krb5.keytab /etc/krb5.keytab
 
+echo "starting nginx..."
+echo "log dir: ./nginx-spnego/data/nginx/logs"
 exec /usr/sbin/nginx
