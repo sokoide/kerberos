ea174a40dedb919a8962b4969eaf792720ed8be7
So Koide <sokoide@gmail.com>
2021-03-13 17:26:58 +0900

    Removed unused /conf

--- docker/nginx-spnego/data/nginx/nginx-start.sh
+++ docker/nginx-spnego/data/nginx/nginx-start.sh
@@ -3,4 +3,5 @@
 cp /data/nginx/conf/nginx.conf /usr/local/nginx/conf
 echo "starting nginx..."
 echo "log dir: ./docker/nginx-spnego/data/nginx/logs"
+rm -rf /data/nginx/logs/*
 exec /usr/local/nginx/sbin/nginx
